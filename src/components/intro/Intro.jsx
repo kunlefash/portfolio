91ff8b797c9cdb584ab423669ea0e86f8cc04611
kunlefash <kunlefashmayop@gmail.com>
2022-01-26 19:43:35 -0400

    initial commit

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,14 @@
+import "./intro.css"
+const Intro = () => {
+  return <div className="i">
+      <div className="i-left">
+        <div className="i-left-wrapper">
+          <h2 className="i-intro">Hello, My name is Fasakin Adekunle</h2>
+          <h2 className="i-name">Fasakin Adekunle </h2>
+        </div>
+      </div>
+      <div className="i-right">right</div>
+  </div>;
+};
+
+export default Intro;
