bda5fcaf54bd7bb91f9e458c7847c8161a94947a
kunlefash <kunlefashmayop@gmail.com>
2022-02-10 08:58:25 -0400

    main

--- src/data.js
+++ src/data.js
@@ -2,31 +2,31 @@ export const products = [
     {
         id: 1,
         img: "https://i.ibb.co/4TD6cYf/1.jpg",
-        link: "http://lama.dev",
+        link: "http://finallytosin.contently.com",
     },
     {
         id: 2,
         img: "https://i.ibb.co/6cKRBfN/2.jpg",
-        link: "http://lama.dev",
+        link: "http://finallytosin.contently.com",
     },
     {
         id: 3,
         img: "https://i.ibb.co/3CtQ86p/3.jpg",
-        link: "http://lama.dev",
+        link: "http://finallytosin.contently.com",
     },
     {
         id: 4,
         img: "https://i.ibb.co/x^JgzjS/4.jpg",
-        link: "http://lama.dev",
+        link: "http://finallytosin.contently.com",
     },
     {
         id: 5,
         img: "https://i.ibb.co/k2j4Zpq/5.jpg",
-        link: "http://lama.dev",
+        link: "http://finallytosin.contently.com",
     },
     {
         id: 6,
         img: "https://i.ibb.co/rw1F4NH/6.jpg",
-        link: "http://lama.dev",
+        link: "http://finallytosin.contently.com",
     },
 ];
